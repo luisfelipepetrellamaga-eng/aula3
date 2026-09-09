1fb37bde33afc06b11d5469459d71045864f6946
luisfelipepetrellamaga-eng <luisfelipepetrellamaga@gmail.com>
2026-09-09 12:38:56 -0300

    Add files via upload

---
+++ Exercicios/exercicio1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class exercicio1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o numero inteiro");
+int numero = entrada.nextInt();
+if (numero>20) {
+    System.out.println("A metade do número é: " + (numero / 2.0));
+}
+entrada.close();
+}
+}
