b07b783df735643094b342e16481648b144f68b6
luisfelipepetrellamaga-eng <luisfelipepetrellamaga@gmail.com>
2026-09-23 11:53:26 -0300

    Add files via upload

---
+++ Exercicios3/Exercicio3.1.java
@@ -0,0 +1,16 @@
+package Exercicios3;
+
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        int numero = 100;
+
+        while (numero <= 200) {
+            System.out.println(numero);
+            numero++; 
+        }
+    }
+}
+    
+    
+
